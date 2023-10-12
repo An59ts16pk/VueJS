6b07dadea013bf863d30fb55e82885d3e5d513bd
AndreK <andre_15-58@mail.ru>
2023-10-12 23:19:36 +0300

    website DZ4

---
+++ webdz4/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <InternoComponent/>
+  <BlogComponent/>
+  <BlogdetailsComponentVue/>
+</template>
+
+
+<script>
+import InternoComponent from './components/InternoComponent.vue';
+import BlogComponent from './components/BlogComponent.vue';
+import BlogdetailsComponentVue from './components/BlogdetailsComponent.vue';
+
+export default {
+  
+  components: {InternoComponent, BlogComponent, BlogdetailsComponentVue}
+}
+</script>
+
+<style lang="scss"></style>
