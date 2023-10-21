477473aa0bf4be41d7748432f40d6eabc35bc48d
AndreK <andre_15-58@mail.ru>
2023-10-21 13:25:16 +0300

    DZ6

---
+++ webdz6/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <InternoComponent/>
+  <BlogComponent/>
+  <BlogdetailsComponent/>
+  <ProjectComponent/>
+</template>
+
+<script>
+import InternoComponent from './components/InternoComponent.vue';
+import BlogComponent from './components/BlogComponent.vue';
+import BlogdetailsComponent from './components/BlogdetailsComponent.vue';
+import ProjectComponent from './components/ProjectComponent.vue';
+
+export default {
+  components: {
+    InternoComponent,
+    BlogComponent,
+    BlogdetailsComponent,
+    ProjectComponent
+},
+};
+</script>
+
+<style lang="scss">
+
+</style>
